7ed2ff7151a2e18c98ea6fe8c53367734cb141c3
ThanhhappyVN <105550865+ThanhhappyVN@users.noreply.github.com>
2022-09-30 20:23:03 +0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,17 @@
+let giohang = {
+    apple:0,
+    banana:0,
+    melon:0,
+}
+function click_apple() {
+    giohang.apple ++
+    console.log(giohang)
+}
+function click_banana() {
+    giohang.banana = giohang.banana + 1
+    console.log(giohang)
+}
+function click_melon() {
+    giohang.melon += 1
+    console.log(giohang)
+}
